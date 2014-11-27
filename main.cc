a53b9aaf7f023b47fa8148985e460e0ac10cb3c0
Gabriel Barsky <gaby.barsky@gmail.com>
2014-11-27 16:41:47 -0500

    modify main to read input

--- main.cc
+++ main.cc
@@ -1,9 +1,20 @@
 #include "board.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
     Board *grid = new Board("grid.txt");
     //grid->modifyLocation(0, 0, 'a');
     //grid->modifyLocation(77, 24, 0, 'c');
     grid->printBoard();
+
+    string c;
+    cin >> c;
+    while(!cin.eof()) {
+        grid->player->move(c);
+        grid->printBoard();
+        cin >> c;
+    }
     return 0;
 }
