99c6d537f082fd4c7dea78b50a29a6610f26c1a8
Gabriel Barsky <gaby.barsky@gmail.com>
2014-11-28 18:57:30 -0500

    intro header

--- main.cc
+++ main.cc
@@ -3,7 +3,16 @@
 #include <string>
 using namespace std;
 
+void printBeginning() {
+    cout << endl <<
+"Welcome to CHAMBER CRAWLER 3000!!!\n\
+This incredible version of the game is\n\
+brought to you by: Gabriel Barsky, Nick White,\n\
+AND CS246. We hope you enjoy." << endl << endl;
+}
+
 int main() {
+    printBeginning();
     Board *grid = new Board("grid.txt");
     //grid->modifyLocation(0, 0, 'a');
     //grid->modifyLocation(77, 24, 0, 'c');
