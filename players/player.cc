12c6bde059a81351c0455e072818cdfd32ab2775
Nick White <nick41496@gmail.com>
2014-11-29 16:22:11 -0500

    Added comments

--- players/player.cc
+++ players/player.cc
@@ -3,6 +3,9 @@
 #include <string>
 using namespace std;
 
+/*
+ * Player constructor, initializes the floor, previous tile, and action
+ */
 Player::Player(int hp, int atk, int def, int chamber, vector<int>&pos, string race)
                 : Character(hp, atk, def, '@', pos),
                   gold(0), chamber(chamber), race(race) {
@@ -11,57 +14,113 @@ Player::Player(int hp, int atk, int def, int chamber, vector<int>&pos, string ra
     action = "Player character has spawned.";
 }
 
+/*
+ * Player destructor
+ */
 Player::~Player() {
 }
 
+/*
+ * Purpose: Getter method for race
+ * Returns: race
+ */
 string Player::getRace() {
     return race;
 }
 
+/*
+ * Purpose: Getter method for floor
+ * Returns: floor
+ */
 int Player::getFloor() {
     return floor;
 }
 
+/*
+ * Purpose: Allows the player to move to the next floor
+ * Returns: Nothing
+ */
 void Player::upFloor() {
     floor++;
 }
 
+/*
+ * Purpose: Getter method for chamber
+ * Returns: chamber
+ */
 int Player::getChamber() {
     return chamber;
 }
 
+/*
+ * Purpose: Setter method for chamber, allows the player to
+ * move between chambers
+ * Returns: Nothing
+ */
 void Player::setChamber(int chamber) {
     this->chamber = chamber;
 }
 
+/*
+ * Purpose: Getter method for action
+ * Returns: action
+ */
 string Player::getAction() {
     return action;
 }
 
+/*
+ * Purpose: Adds new text to the action string
+ * Returns: Nothing
+ */
 void Player::addAction(string action) {
     this->action = this->action + action;
 }
 
+/*
+ * Purpose: Getter method for prevTile
+ * Returns: prevTile
+ */
 char Player::getPrevTile() {
     return prevTile;
 }
 
+/*
+ * Purpose: Setter method for prevTile
+ * Returns: Nothing
+ */
 void Player::setPrevTile(char tile) {
     prevTile = tile;
 }
 
+/*
+ * Purpose: Setter method for position
+ * Returns: Nothing
+ */
 void Player::setPosition(vector<int> pos) {
     position = pos;
 }
 
+/*
+ * Purpose: Adds gold to the player's stash
+ * Returns: Nothing
+ */
 void Player::addGold(int quantity) {
     gold += quantity;
 }
 
+/*
+ * Purpose: Getter method for gold
+ * Returns: gold
+ */
 int Player::getGold() {
     return gold;
 }
 
+/*
+ * Purpose: Moves the player in the specified direction
+ * Returns: Nothing
+ */
 void Player::move(string direction) {
     if(direction == "no") {
         position[1] -= 1;
